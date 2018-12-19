94290a0fe4bdb4bbf3f74a169d698260115928b4
Alyssa Voccia <avoccia@hotmail.com>
2018-12-19 18:53:10 -0500

    Updated variable in main.js

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 =======================*/
 
 $(document).ready(function() {
-    var images = document.getElementsByTagName('a');
+    let images = document.getElementsByTagName('a');
     $('#search-input').on('keyup', function() {
         // Stores users input into search variable
-        var search = $(this).val().toLowerCase();
+        let $search = $(this).val().toLowerCase();
         // Check for string in the description
         for (i = 0; i < images.length; i++) {
             textVal = images[i].getAttribute('data-caption');
-            if (textVal.toLowerCase().indexOf(search) > -1) {
+            if (textVal.toLowerCase().indexOf($search) > -1) {
                 images[i].style.display = '';
             } else {
                 images[i].style.display = 'none';
